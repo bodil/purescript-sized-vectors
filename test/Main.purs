262e0e5a96396de2c0a18dfa74d5f0393cd98a50
Matthew Leon <ml@matthewleon.com>
2017-04-26 11:53:34 +0000

    prime' fns without redundant args

--- test/Main.purs
+++ test/Main.purs
@@ -4,8 +4,8 @@ import Control.Monad.Aff.AVar (AVAR)
 import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Console (CONSOLE)
 import Data.Traversable (sequence)
-import Data.Typelevel.Num (toInt, d6, d3, d9, d2, D9)
-import Data.Vec (Vec, slice, tail, drop, take, lengthT, concat, replicate, (+>), empty)
+import Data.Typelevel.Num (D1, D2, D3, D9, d2, d3, d6, toInt)
+import Data.Vec (Vec, concat, drop, drop', empty, length, lengthT, replicate, replicate', slice, slice', tail, take, take', (+>))
 import Prelude (($), Unit, pure, discard)
 import Test.Unit (suite, test)
 import Test.Unit.Assert (equal)
@@ -17,21 +17,26 @@ main = runTest do
   suite "vec" do
     let vec1 = replicate d2 1
         vec2 = replicate d3 2
-        vec3 = replicate d9 3
+        vec3 = (replicate' 3 :: Vec D9 Int)
     test "cons length" do
       equal 3 $ toInt $ lengthT $ 1 +> 2 +> 3 +> empty
+      equal 3 $ length $ 1 +> 2 +> 3 +> empty
     test "replicate length" do
       equal 2 $ toInt $ lengthT vec1
+      equal 9 $ length vec3
     test "concat length" do
       equal 5 $ toInt $ lengthT (concat vec1 vec2)
     test "take length" do
-      equal 2 $ toInt $ lengthT (take d2 vec2)
+      equal 2 $ length $ take d2 vec2
+      equal 2 $ toInt $ lengthT (take' vec2) :: D2
     test "drop length" do
-      equal 1 $ toInt $ lengthT (drop d2 vec2)
+      equal 1 $ length $ drop d2 vec2
+      equal 1 $ toInt $ lengthT (drop' vec2) :: D1
     test "tail length" do
       equal 1 $ toInt $ lengthT (tail vec1)
     test "slice length" do
-      equal 3 $ toInt $ lengthT (slice d3 d6 vec3)
+      equal 3 $ length $ slice d3 d6 vec3
+      equal 3 $ toInt $ lengthT (slice' d3 vec3) :: D3
     test "pure replicates" do
       let vec3' = pure 3 :: Vec D9 Int
       equal vec3 vec3'
